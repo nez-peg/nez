bfa666b440add74362f4a5e1ba23fffc6e11dfd5
kensuketamura <kensuke.tamura.183@gmail.com>
2015-05-21 19:40:32 +0900

    design FSharpGenerator

--- src/nez/fsharp/FSharpGenerator.java
+++ src/nez/fsharp/FSharpGenerator.java
@@ -1,11 +1,59 @@
 package nez.fsharp;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import nez.ast.Tag;
 
 
 public class FSharpGenerator extends SourceGenerator {
+	
+	private static boolean UseExtend;
+	/**List of scopes in JavaScript. Finally, we output information which this list has.**/
+	private ArrayList<FSharpScope> fsClasses;
+	
+	public FSharpGenerator(){
+		FSharpGenerator.UseExtend = false;
+	}
+	
+	/**Find recursively JavaScript Function and Object from ModifiableTree, and assign them to fsClasses**/
+	private void findScope(ModifiableTree node, FSharpScope currentScope){
+		FSharpScope nextScope = currentScope;
+		if(node.is(JSTag.TAG_FUNC_DECL)){
+			String funcName = node.get(2).getText();
+			fsClasses.add(new FSharpScope(funcName));
+		} else if (node.is(JSTag.TAG_OBJECT)){
+			
+		}
+		
+		for(int child_i = 0; child_i < node.size(); child_i++){
+			findScope(node.get(child_i), nextScope);
+		}
+		//TODO
+	}
+	
+	/**format ModifiedTrees, which is the flow of curerntScope operation in JavaScript program, 
+	 * for F# code generation**/
+	private void formatTree(FSharpScope currentScope){
+		//TODO
+	}
+	
+	private void generateFSCode(FSharpScope currentScope){
+		//TODO
+	}
+	
+	private void generatePrintCode(){
+		//TODO
+	}
+	
+	public void toSource(ModifiableTree node){
+		findScope(node, null);
+		for(FSharpScope fsClass : fsClasses){
+			formatTree(fsClass);
+		}
+		for(FSharpScope fsClass : fsClasses){
+			generateFSCode(fsClass);
+		}
+		generatePrintCode();
+	}
 	
 }
