3472a90c1a9a009e0d498baf434cc7739372d81c
shun-honda <shun.honda.suffix@gmail.com>
2015-08-21 14:24:32 +0900

    fixed JCodeTree

--- src/nez/ast/jcode/JCodeTree.java
+++ src/nez/ast/jcode/JCodeTree.java
@@ -1,18 +1,11 @@
 package nez.ast.jcode;
 
-import nez.ast.AbstractTree;
-import nez.ast.Source;
 import nez.ast.Tag;
 
-public class JCodeTree extends AbstractTree<JCodeTree> {
-	Class<?> typed;
-	public JCodeTree(Tag tag, Source source, long pos, int len, int size, Object value) {
-		super(tag, source, pos, len, size > 0 ? new JCodeTree[size] : null, value);
-	}
-	@Override
-	protected JCodeTree dupImpl() {
-		return new JCodeTree(this.getTag(), 
-			this.getSource(), this.getSourcePosition(), this.getLength(), this.size(), getValue());
-	}
+public interface JCodeTree<T> {
+	public Class<?> getTypedClass();
 
+	public T dup();
+
+	public Tag getTag();
 }
