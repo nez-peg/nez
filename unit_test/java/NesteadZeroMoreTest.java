c0a645bc49ada2d7c384317af61dc6800b1508b3
kkuramitsu <kimio@konohascript.org>
2015-09-14 21:43:23 +0900

    refactored Combinator

--- unit_test/java/NesteadZeroMoreTest.java
+++ unit_test/java/NesteadZeroMoreTest.java
@@ -1,21 +1,19 @@
+import java.io.IOException;
+
 import nez.main.Command;
+import nez.main.CommandContext;
 
 import org.junit.Test;
 
-
 public class NesteadZeroMoreTest {
-	@Test(timeout=5000)
-	public void test() {
-//		final String pegRule = 
-//				"File = B*\n" +
-//				"B = '1'*";
-//		final String[] args = new String[] {
-//				"parse", "-t", "111", "-e", pegRule	
-//		};
-//		CommandConfigulation config = new CommandConfigulation();
-//		config.parseCommandOption(args);
-//		Command com = config.getCommand();
-//		com.exec(config);
-//		// FIXME assert
+	@Test(timeout = 5000)
+	public void test() throws IOException {
+		final String pegRule = "File = B*\n" + "B = '1'*";
+		final String[] args = new String[] { "parse", "-t", "111", "-e", pegRule };
+		CommandContext config = new CommandContext();
+		config.parseCommandOption(args);
+		Command com = config.newCommand();
+		com.exec(config);
+		// FIXME assert
 	}
 }
