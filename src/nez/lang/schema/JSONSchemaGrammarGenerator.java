7d54303ddb299e987528a5dd3604226d888b353a
yshinya6 <y.shinya.kml@gmail.com>
2015-09-23 19:44:31 +0900

    added implementation to abstract method

--- src/nez/lang/schema/JSONSchemaGrammarGenerator.java
+++ src/nez/lang/schema/JSONSchemaGrammarGenerator.java
@@ -1,130 +1,153 @@
 package nez.lang.schema;
 
-import java.util.List;
-
-import nez.Grammar;
-import nez.ast.Tree;
+import nez.lang.Expression;
+import nez.lang.GrammarFile;
 
 public class JSONSchemaGrammarGenerator extends SchemaGrammarGenerator {
 
-	private Grammar gfile;
-
-	public JSONSchemaGrammarGenerator(Grammar gfile) {
+	public JSONSchemaGrammarGenerator(GrammarFile gfile) {
 		this.gfile = gfile;
 	}
 
 	@Override
 	public void loadPredefinedRules() {
-		// TODO Auto-generated method stub
-
+		JSONPredefinedGrammar pre = new JSONPredefinedGrammar(gfile);
+		pre.define();
 	}
 
 	@Override
 	public void newRoot(String structName) {
-		// TODO Auto-generated method stub
-
+		Expression root = _NonTerminal(structName);
+		Expression[] seq = { _NonTerminal("VALUESEP"), root };
+		Expression[] array = { _OpenSquare(), _S(), root, _S(), gfile.newRepetition1(seq), _S(), _CloseSquare() };
+		gfile.addProduction(null, "Root", gfile.newChoice(gfile.newSequence(root), gfile.newSequence(array)));
 	}
 
 	@Override
-	public Element newElement(String name, Type t) {
-		// TODO Auto-generated method stub
-		return null;
+	public void newElement(Type t) {
+		Expression[] l = { _DQuat(), t.getTypeExpression(), _DQuat(), _NonTerminal("NAMESEP"), t.next().getTypeExpression(), gfile.newOption(_NonTerminal("VALUESEP")) };
+		gfile.addProduction(null, t.getElementName(), gfile.newSequence(l));
 	}
 
 	@Override
-	public void newStruct(String name, Type t) {
-		// TODO Auto-generated method stub
-
+	public void newStruct(String structName, Type t) {
+		Expression[] l = { _OpenWave(), t.getTypeExpression(), _CloseWave() };
+		gfile.addProduction(null, structName, gfile.newSequence(l));
 	}
 
 	@Override
-	public void newMembers(String structName, Type... types) {
-		// TODO Auto-generated method stub
-
+	public void newMembers(Type... types) {
+		Expression[] l = new Expression[types.length];
+		int index = 0;
+		for (Type type : types) {
+			l[index++] = type.getTypeExpression();
+		}
+		gfile.addProduction(null, getTableName() + "_Members", gfile.newChoice(l));
 	}
 
 	@Override
-	public Type newOption(Type t) {
-		// TODO Auto-generated method stub
-		return null;
+	public Type newRequired(String name, Type t) {
+		Expression def = _Def(getTableName(), name);
+		return new Type(name, def, t);
 	}
 
 	@Override
-	public Type newRequired(Type t) {
-		// TODO Auto-generated method stub
-		return null;
+	public Type newOption(String name, Type t) {
+		Expression expr = _String(name);
+		return new Type(name, expr, t);
 	}
 
 	@Override
 	public Type newTObject() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Type(_NonTerminal("JSONObject"));
 	}
 
 	@Override
-	public Type newTStruct() {
-		// TODO Auto-generated method stub
-		return null;
+	public Type newTStruct(String structName) {
+		return new Type(_NonTerminal(structName));
 	}
 
 	@Override
 	public Type newTArray(Type t) {
-		// TODO Auto-generated method stub
-		return null;
+		Expression tExpr = t.getTypeExpression();
+		Expression[] array = { _OpenSquare(), tExpr, gfile.newRepetition(_NonTerminal("VALUESEP"), tExpr), _CloseSquare() };
+		return new Type(gfile.newSequence(array));
 	}
 
 	@Override
-	public Type newTEnum(Tree<?> node) {
-		// TODO Auto-generated method stub
-		return null;
+	public Type newTEnum(String[] candidates) {
+		Expression[] choice = new Expression[candidates.length];
+		int index = 0;
+		for (String cand : candidates) {
+			choice[index++] = _String(cand);
+		}
+		return new Type(gfile.newChoice(choice));
 	}
 
 	@Override
 	public Type newTInteger() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Type(_NonTerminal("INT"));
 	}
 
 	@Override
 	public Type newTFloat() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Type(_NonTerminal("Number"));
 	}
 
 	@Override
 	public Type newTString() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Type(_NonTerminal("String"));
 	}
 
 	@Override
 	public Type newTAny() {
+		return new Type(_NonTerminal("Any"));
+	}
+
+	// FIXME
+	@Override
+	public Type newSet() {
+		String tableName = getTableName();
+		Expression[] l = new Expression[getRequiredList().size() + 1];
+		int index = 1;
+		l[0] = gfile.newRepetition(_NonTerminal(getTableName() + "_Members"));
+		for (String required : getRequiredList()) {
+			l[index++] = gfile.newExists(tableName, required);
+		}
+		return new Type(gfile.newLocal(tableName, gfile.newSequence(l)));
+	}
+
+	@Override
+	public Type newUniq(String elementName) {
+		Expression expr = gfile.newSequence(gfile.newNot(gfile.newExists("T" + getTableCounter(), elementName)), _NonTerminal(elementName));
+		return new Type(expr);
+	}
+
+	@Override
+	public Type newOthers() {
+		return new Type(_NonTerminal("Member"));
+	}
+
+	@Override
+	public Type newPermutation() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public Type newSet(String table, List<String> list) {
-		// TODO Auto-generated method stub
-		return null;
+	private final Expression _OpenSquare() {
+		return gfile.newByteChar('[');
 	}
 
-	@Override
-	public Type newPermutation(List<String> required, List<String> implied) {
-		// TODO Auto-generated method stub
-		return null;
+	private final Expression _CloseSquare() {
+		return gfile.newByteChar(']');
 	}
 
-	@Override
-	public Type newUniq(String table, String elementName) {
-		// TODO Auto-generated method stub
-		return null;
+	private final Expression _OpenWave() {
+		return gfile.newByteChar('{');
 	}
 
-	@Override
-	public Type newOtherAny(String table, List<String> members) {
-		// TODO Auto-generated method stub
-		return null;
+	private final Expression _CloseWave() {
+		return gfile.newByteChar('}');
 	}
 
 }
