e68cb4f96e6400e26be675c9f463bb3cbf02ebcb
yshinya6 <y.shinya.kml@gmail.com>
2015-09-23 19:46:06 +0900

    added getter

--- src/nez/lang/schema/Type.java
+++ src/nez/lang/schema/Type.java
@@ -4,12 +4,28 @@ import nez.lang.Expression;
 
 public class Type {
 	private Expression typeExpression;
+	private String elementName;
+	private Type next;
 
-	public Type(Expression t) {
-		this.typeExpression = t;
+	public Type(String name, Expression e, Type t) {
+		this.elementName = name;
+		this.typeExpression = e;
+		this.next = t;
+	}
+
+	public Type(Expression e) {
+		this.typeExpression = e;
+	}
+
+	public String getElementName() {
+		return this.elementName;
 	}
 
 	public Expression getTypeExpression() {
-		return typeExpression;
+		return this.typeExpression;
+	}
+
+	public Type next() {
+		return this.next;
 	}
 }
