e7ff87ed08e4cdc8281f659df8c2b7af29c7c23a
kkuramitsu <kimio@konohascript.org>
2015-09-24 16:01:10 +0900

    added mozdump command

---
+++ src/nez/ext/Cmozdump.java
@@ -0,0 +1,24 @@
+package nez.ext;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import nez.main.Command;
+import nez.main.CommandContext;
+import nez.parser.vm.Moz;
+
+public class Cmozdump extends Command {
+	@Override
+	public void exec(CommandContext config) throws IOException {
+		File f = new File(config.getGrammarPath());
+		byte[] buf = new byte[(int) f.length()];
+		FileInputStream fis = new FileInputStream(f);
+		fis.read(buf);
+		fis.close();
+
+		System.out.println("Moz dump");
+		Moz.dump(buf);
+	}
+
+}
