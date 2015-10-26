65f02e33df4468b9ca3cd8b9020a3e7573d4f212
shun-honda <shun.honda.suffix@gmail.com>
2015-10-26 16:41:44 +0900

    fixed build error

--- src/nez/parser/vm/MozCompiler.java
+++ src/nez/parser/vm/MozCompiler.java
@@ -33,168 +33,196 @@ public class MozCompiler extends VisitorMap<DefaultVisitor> {
 	public class Pempty extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Pfail extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Cbyte extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Cany extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Cset extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Cmulti extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Pchoice extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Psequence extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Poption extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Pzero extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Pone extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Pand extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Pnot extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Tnew extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Tlink extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Tlfold extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Ttag extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Treplace extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Tcapture extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Tdetree extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Xblock extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Xlocal extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Xif extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Xon extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Xsymbol extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Xexisits extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Xmatch extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
 	public class Xis extends DefaultVisitor {
 		@Override
 		public Instruction accept(Expression e, Instruction next) {
+			return next;
 		}
 	}
 
