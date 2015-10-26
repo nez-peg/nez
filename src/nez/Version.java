1b68886cb0fb7df026cfa4840720cbc0339e4564
kkuramitsu <kimio@konohascript.org>
2015-10-26 16:16:04 +0900

    moved version info

--- src/nez/Version.java
+++ src/nez/Version.java
@@ -3,9 +3,9 @@ package nez;
 public class Version {
 	public final static int MainVersion = 1;
 	public final static int MinorVersion = 0;
-	public final static int REV=792;
+	public final static int REV=793;
 	public final static boolean ReleasePreview = true;
-	public final static String Version = "" + MajorVersion + "." + MinorVersion + "-" + REV;
+	public final static String Version = "" + MainVersion + "." + MinorVersion + "-" + REV;
 	public final static String Copyright = "Copyright (c) 2014-2015, Nez project authors";
 	public final static String License = "BSD-Licensed Open Source";
 
