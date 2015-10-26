d37ec3659ce6898669dfac6388b1a2c195bd654b
kkuramitsu <kimio@konohascript.org>
2015-10-26 12:22:19 +0900

    removed nez.main.ext

--- src/nez/main/ext/Ljavanez.java
+++
@@ -1,9 +0,0 @@
-package nez.main.ext;
-
-import nez.x.generator.GeneratorLoader;
-
-public class Ljavanez {
-	static {
-		GeneratorLoader.regist("javanez", nez.x.generator.CombinatorGenerator.class);
-	}
-}
--- src/nez/main/ext/Lpegtl.java
+++
@@ -1,9 +0,0 @@
-package nez.main.ext;
-
-import nez.x.generator.GeneratorLoader;
-
-public class Lpegtl {
-	static {
-		GeneratorLoader.regist("pegtl", nez.x.generator.PEGTLGenerator.class);
-	}
-}
