da09095e611c6cbf1d683011b756a3b3837af23b
yshinya6 <y.shinya.kml@gmail.com>
2015-12-22 16:43:25 +0900

    modified GrammarLoader

--- src/nez/lang/ast/GrammarLoaderExtension.java
+++ src/nez/lang/ast/GrammarLoaderExtension.java
@@ -1,9 +1,15 @@
 package nez.lang.ast;
 
+import nez.lang.Grammar;
+
 public abstract class GrammarLoaderExtension implements GrammarLoaderVisitor {
 	protected final GrammarLoader loader;
 
 	public GrammarLoaderExtension(GrammarLoader loader) {
 		this.loader = loader;
+	}
+
+	public final Grammar getGrammar() {
+		return this.loader.getGrammar();
 	}
 }
